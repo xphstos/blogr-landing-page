da46b922b45669eb21dd45f2e4eca7eddfb7c16c
Chris Gotsikas <chris_gotsikas@192.168.1.3>
2024-06-13 23:25:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Frontend Mentor - Blogr landing page
+
+![Design preview for the Blogr landing page coding challenge](./desktop-preview.jpg)
+
+This is a solution to the [Blogr landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP).
+
+### links
+
+- solution url: [You're here](https://github.com/xphstos/blogr-landing-page)
+- live site url: [add live site url here]()
---
+++ src/data/data.ts
@@ -0,0 +1,65 @@
+export const LINKS = [
+  {
+    name: "Product",
+    links: [
+      {
+        label: "Overview",
+        href: "#",
+      },
+      {
+        label: "Pricing",
+        href: "#",
+      },
+      {
+        label: "Marketplace",
+        href: "#",
+      },
+      {
+        label: "Features",
+        href: "#",
+      },
+      {
+        label: "Integrations",
+        href: "#",
+      },
+    ],
+  },
+  {
+    name: "Company",
+    links: [
+      {
+        label: "About",
+        href: "#",
+      },
+      {
+        label: "Team",
+        href: "#",
+      },
+      {
+        label: "Blog",
+        href: "#",
+      },
+      {
+        label: "Careers",
+        href: "#",
+      },
+    ],
+  },
+  {
+    name: "Connect",
+    links: [
+      {
+        label: "Contact",
+        href: "#",
+      },
+      {
+        label: "Newsletter",
+        href: "#",
+      },
+      {
+        label: "LinkedIn",
+        href: "#",
+      },
+    ],
+  },
+];
